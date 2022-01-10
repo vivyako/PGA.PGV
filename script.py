0df4cb485b1189cbf687bf1a4408199277a5f405
vivyako <72323190+vivyako@users.noreply.github.com>
2022-01-10 11:13:18 +1100

    Body of code using OOP

--- script.py
+++ script.py
@@ -1,38 +1,63 @@
-valuesVertical = []
-valuesHorizontal = []
-valuesMain = []
-maxHorizontal = 0
+from _typeshed import Self
+events = []
 
-def Avg(arr):
-    return sum(arr) / len(arr)
+class Event(object):
 
-def Filling(line):
-    str = ''
-    result = 0
-    for i in range(31, 40):
-        if line[i] == ' ':
-            continue
-        else:
-            str += line[i]
-    result = float(str)
-    return result
+    stations = []    
+    
+    def addingValues(self, line):
+        Event.stations.append(Station())
+        if 'E   ' in line:
+            print()
 
+class Station(object):
+
+    valuesMain = []
+    valuesVertical = []
+    valuesHorizontal = []
+    stationType = '' # A - ускорение, V - скорость
+    stationName = ''
+
+    def Filling(self, line):
+        str = ''
+        result = 0
+        for i in range(31, 40):
+            if line[i] == ' ':
+                continue
+            else:
+                str += line[i]
+        result = float(str)
+        return result
+
+events.append(Event())
+j = len(events) - 1
 with open('data.txt', 'r') as f:
     for line in f:
-        if 'E   ' in line:
-            if line[7] == 'Z':
-                valuesVertical.append(Filling(line))
-            elif line[7] == 'E':
-                valuesHorizontal.append(Filling(line))
-                if valuesHorizontal[len(valuesHorizontal) - 1] > valuesHorizontal[len(valuesHorizontal) - 2]:
-                    maxHorizontal = valuesHorizontal[len(valuesHorizontal) - 1]
-                else:
-                    maxHorizontal = valuesHorizontal[len(valuesHorizontal) - 2]
-                temp = max(valuesHorizontal[len(valuesHorizontal) - 1], valuesHorizontal[len(valuesHorizontal) - 2])
-                valuesMain.append(valuesVertical[len(valuesVertical) - 1] / maxHorizontal)
-            else:
-                valuesHorizontal.append(Filling(line))
-print('Average PGV: ', Avg(valuesMain))
+        if line[0] != ' ':
+            print(1)
+        else:
+            events.append(Event())
+            j += 1
+
+#СМОТРЕТЬ СЮДА, КАК НА ПРИМЕР:
+# with open('data.txt', 'r') as f:
+#     for line in f:
+#         events.append(Event())
+#         if 'E   ' in line:
+#             if line[7] == 'Z':
+#                 valuesVertical.append(events.Filling(line))
+#             elif line[7] == 'E':
+#                 valuesHorizontal.append(events.Filling(line))
+#                 if valuesHorizontal[len(valuesHorizontal) - 1] > valuesHorizontal[len(valuesHorizontal) - 2]:
+#                     maxHorizontal = valuesHorizontal[len(valuesHorizontal) - 1]
+#                 else:
+#                     maxHorizontal = valuesHorizontal[len(valuesHorizontal) - 2]
+#                 temp = max(valuesHorizontal[len(valuesHorizontal) - 1], valuesHorizontal[len(valuesHorizontal) - 2])
+#                 valuesMain.append(valuesVertical[len(valuesVertical) - 1] / maxHorizontal)
+#                 events.main = valuesMain[len(valuesMain) - 1]
+#             else:
+#                 valuesHorizontal.append(events.Filling(line))
+#print('Average PGV: ', Avg(valuesMain))
 # line[7] - определяем вертикаль или горизонталь
 # можно забрать значения начиная с 28 символа, пока не наткнешься СНОВА на пустой символ, а потом пустые символы удалить
 # выбираем максимальную из горизонтальных, потом вертикальную делим на горизонтальную
